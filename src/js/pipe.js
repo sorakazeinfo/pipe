2610c3809da1b1891375a818e3f6351f0f9518d2
Hyte Sorakaze <github@sorakaze.info>
2020-04-27 23:58:18 +0900

    Sort alphabet asc

--- src/js/pipe.js
+++ src/js/pipe.js
@@ -1,11 +1,11 @@
 import Modal from "./components/modal";
-import TextCounter from "./components/text_counter";
 import Scroll from "./components/scroll";
 import StretchTextarea from "./components/stretch_textarea";
 import Tab from "./components/tab";
+import TextCounter from "./components/text_counter";
 
 (new Modal()).initialize();
 (new Scroll()).initialize();
-(new TextCounter()).initialize();
 (new StretchTextarea()).initialize();
 (new Tab()).initialize();
+(new TextCounter()).initialize();
