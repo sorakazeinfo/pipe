fe312ebd966cbc3973160b3ed63b4740bb6e3e9f
Hyte Sorakaze <github@sorakaze.info>
2020-06-19 23:59:01 +0900

    #22 Added '.pp-footer__horizontal-menu' style

--- docs/src/components/footer.vue
+++ docs/src/components/footer.vue
@@ -3,6 +3,12 @@
 		<h1>Footer</h1>
 
 		<div class="pp-footer">
+			<ul class="pp-footer__horizontal-menu">
+				<li><a href="#">About us</a></li>
+				<li><a href="#">Term of use</a></li>
+				<li><a href="#">Privacy Policy</a></li>
+				<li><a href="#">Inquiry</a></li>
+			</ul>
 			<p>this is footer section</p>
 			<p>(C) 2020 Pipe Project</p>
 		</div>
