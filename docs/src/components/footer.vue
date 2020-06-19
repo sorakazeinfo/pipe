6faf2ba99d330beb397350fe1e3b854550a17e84
Hyte Sorakaze <github@sorakaze.info>
2020-06-20 00:20:46 +0900

    #23 Added '.pp-footer__vertical-menu' style

--- docs/src/components/footer.vue
+++ docs/src/components/footer.vue
@@ -9,6 +9,41 @@
 				<li><a href="#">Privacy Policy</a></li>
 				<li><a href="#">Inquiry</a></li>
 			</ul>
+
+			<div class="pp-footer__vertical-menu">
+				<ul class="pp-footer__vertical-menu__list">
+					<li class="pp-footer__vertical-menu__list__caption">Caption</li>
+					<li><a href="#">About us</a></li>
+					<li><a href="#">Term of use</a></li>
+					<li><a href="#">Privacy Policy</a></li>
+					<li><a href="#">Inquiry</a></li>
+				</ul>
+
+				<ul class="pp-footer__vertical-menu__list">
+					<li class="pp-footer__vertical-menu__list__caption">Caption</li>
+					<li><a href="#">About us</a></li>
+					<li><a href="#">Term of use</a></li>
+					<li><a href="#">Privacy Policy</a></li>
+					<li><a href="#">Inquiry</a></li>
+				</ul>
+
+				<ul class="pp-footer__vertical-menu__list">
+					<li class="pp-footer__vertical-menu__list__caption">Caption</li>
+					<li><a href="#">About us</a></li>
+					<li><a href="#">Term of use</a></li>
+					<li><a href="#">Privacy Policy</a></li>
+					<li><a href="#">Inquiry</a></li>
+				</ul>
+
+				<ul class="pp-footer__vertical-menu__list">
+					<li class="pp-footer__vertical-menu__list__caption">Caption</li>
+					<li><a href="#">About us</a></li>
+					<li><a href="#">Term of use</a></li>
+					<li><a href="#">Privacy Policy</a></li>
+					<li><a href="#">Inquiry</a></li>
+				</ul>
+			</div>
+
 			<p>this is footer section</p>
 			<p>(C) 2020 Pipe Project</p>
 		</div>
