0e73d960b8cb55f4d14f4e3a5bcc402ac9fab98e
Hyte Sorakaze <github@sorakaze.info>
2020-07-05 22:46:13 +0900

    #28 Add .pp-tab-border style

--- src/js/components/tab.js
+++ src/js/components/tab.js
@@ -23,16 +23,24 @@ class TabContainer {
 
 		this.itemElements = [];
 		this.contentElements = [];
+
+		if (tab.classList.contains("pp-tab")) {
+			this.activeClassName = "pp-tab__menu__active";
+			this.menuItemSelector = ".pp-tab__menu li";
+		} else if (tab.classList.contains("pp-tab-border")) {
+			this.activeClassName = "pp-tab-border__menu__active";
+			this.menuItemSelector = ".pp-tab-border__menu li";
+		}
 	}
 
 	toggle(activeItemID) {
-		this.tabItems.forEach(function(tabItem) {
+		this.tabItems.forEach((tabItem) => {
 			if (tabItem.id === activeItemID) {
-				tabItem.menu.classList.add("pp-tab__menu__active");
+				tabItem.menu.classList.add(this.activeClassName);
 				tabItem.content.style.display = "block";
 				tabItem.visible = true;
 			} else {
-				tabItem.menu.classList.remove("pp-tab__menu__active");
+				tabItem.menu.classList.remove(this.activeClassName);
 				tabItem.content.style.display = "none";
 				tabItem.visible = false;
 			}
@@ -41,13 +49,13 @@ class TabContainer {
 
 	initialize() {
 		// Collect
-		const menuItems = this.rootElement.querySelectorAll(".pp-tab__menu li");
+		const menuItems = this.rootElement.querySelectorAll(this.menuItemSelector);
 		menuItems.forEach(item => {
 			this.tabItems.push({
 				id: Math.random().toString(18).slice(-8),
 				menu: item,
 				content: this.rootElement.querySelector(item.firstElementChild.hash),
-				visible: item.classList.contains("pp-tab__menu__active")
+				visible: item.classList.contains(this.activeClassName)
 			});
 		});
 
