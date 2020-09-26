f81bc5a3216f9a7db7cca510490c7d3fdc9dad5d
Hyte Sorakaze <github@sorakaze.info>
2020-09-27 00:34:44 +0900

    #42 Create 'see-more' component sample

---
+++ docs/src/components/see_more.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1>See more</h1>
+
+		<div>
+			<a href="#" data-pipejs="see-more" data-pipe-target="#seemore">See more</a>
+			<div id="seemore" style="display: none;">
+				more texts...
+			</div>
+		</div>
+
+		<div>
+			<a href="#" data-pipejs="see-more" data-pipe-target="#seemore2">See more</a>
+			<div id="seemore2" class="pp-hidden">
+				more texts...
+			</div>
+		</div>
+	</div>
+</template>
