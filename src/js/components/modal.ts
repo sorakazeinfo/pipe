e50c8923ff724aaf260dfb00dd75dc7e9b4400ea
Hyte Sorakaze <github@sorakaze.info>
2020-09-26 23:51:49 +0900

    Indent down

--- src/js/components/modal.ts
+++ src/js/components/modal.ts
@@ -7,55 +7,53 @@ export default class Modal extends AppComponent implements ComponentInterface {
 	public initialize(): void {
 		const triggerElements: NodeListOf<HTMLElement> = this.getTriggerElements();
 
-		if (triggerElements.length > 0) {
-			triggerElements.forEach(button => {
-				const id = button.getAttribute("data-pipe-target");
-				const modal: HTMLElement = document.querySelector(id);
+		triggerElements.forEach(button => {
+			const id = button.getAttribute("data-pipe-target");
+			const modal: HTMLElement = document.querySelector(id);
 
-				if (modal !== null) {
-					// Trigger
-					button.addEventListener("click", function(e) {
+			if (modal !== null) {
+				// Trigger
+				button.addEventListener("click", function(e) {
+					e.preventDefault();
+					modal.style.display = "block";
+				});
+
+				// Overlay
+				const overlay = modal.querySelector(".pp-modal__overlay");
+				if (overlay !== null) {
+					overlay.addEventListener("click", function(e) {
 						e.preventDefault();
-						modal.style.display = "block";
+						modal.style.display = "none";
 					});
-
-					// Overlay
-					const overlay = modal.querySelector(".pp-modal__overlay");
-					if (overlay !== null) {
-						overlay.addEventListener("click", function(e) {
-							e.preventDefault();
-							modal.style.display = "none";
-						});
-					}
-
-					// Close
-					const overlayClose = modal.querySelector(".pp-modal__close");
-					if (overlayClose !== null) {
-						overlayClose.addEventListener("click", function(e) {
-							e.preventDefault();
-							modal.style.display = "none";
-						});
-					}
-
-					// Close in contents
-					const contentClose = modal.querySelector(".pp-modal__contents__close");
-					if (contentClose !== null) {
-						contentClose.addEventListener("click", function(e) {
-							e.preventDefault();
-							modal.style.display = "none";
-						});
-					}
-
-					// Close Action
-					const closeAction = modal.querySelector(".pp-modal__close-action");
-					if (closeAction !== null) {
-						closeAction.addEventListener("click", (e: Event) => {
-							e.preventDefault();
-							modal.style.display = "none";
-						});
-					}
 				}
-			});
-		}
+
+				// Close
+				const overlayClose = modal.querySelector(".pp-modal__close");
+				if (overlayClose !== null) {
+					overlayClose.addEventListener("click", function(e) {
+						e.preventDefault();
+						modal.style.display = "none";
+					});
+				}
+
+				// Close in contents
+				const contentClose = modal.querySelector(".pp-modal__contents__close");
+				if (contentClose !== null) {
+					contentClose.addEventListener("click", function(e) {
+						e.preventDefault();
+						modal.style.display = "none";
+					});
+				}
+
+				// Close Action
+				const closeAction = modal.querySelector(".pp-modal__close-action");
+				if (closeAction !== null) {
+					closeAction.addEventListener("click", (e: Event) => {
+						e.preventDefault();
+						modal.style.display = "none";
+					});
+				}
+			}
+		});
 	}
 }
--- src/js/components/to_scroll.ts
+++ src/js/components/to_scroll.ts
@@ -7,23 +7,23 @@ export default class ToScroll extends AppComponent implements ComponentInterface
 	public initialize(): void {
 		const triggerElements: NodeListOf<HTMLElement> = this.getTriggerElements();
 
-			triggerElements.forEach(trigger => {
-				const href  = trigger.getAttribute("href");
-				const block = trigger.getAttribute("data-pipe-target") || "start";
+		triggerElements.forEach(trigger => {
+			const href  = trigger.getAttribute("href");
+			const block = trigger.getAttribute("data-pipe-target") || "start";
 
-				if (href !== null && document.querySelector(href) === null) {
-					return;
-				}
+			if (href !== null && document.querySelector(href) === null) {
+				return;
+			}
 
-				const destinationElement = document.querySelector(href) || document.getElementsByTagName("body")[0];
+			const destinationElement = document.querySelector(href) || document.getElementsByTagName("body")[0];
 
-				trigger.addEventListener("click", function(e) {
-					e.preventDefault();
-					destinationElement.scrollIntoView({
-						behavior: "smooth",
-						block: "start"
-					});
+			trigger.addEventListener("click", function(e) {
+				e.preventDefault();
+				destinationElement.scrollIntoView({
+					behavior: "smooth",
+					block: "start"
 				});
 			});
+		});
 	}
 }
