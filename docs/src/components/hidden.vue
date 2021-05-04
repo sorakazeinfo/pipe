9b6cb406adeb5025b1252d70e51e557b49ed3646
Hyte Sorakaze <github@sorakaze.info>
2021-05-04 12:27:58 +0900

    Support Dark Mode

--- docs/src/components/hidden.vue
+++ docs/src/components/hidden.vue
@@ -4,30 +4,33 @@
 
 		<section>
 			<h2>.pp-hidden</h2>
-			<p style="background-color: #eee; padding: 1rem 0.5rem;">
-				<span class="pp-hidden">This message is hidden.</span>&nbsp;
-			</p>
+			<p class="vue-hidden-p"><span class="pp-hidden">This message is hidden.</span>&nbsp;</p>
 		</section>
 
 		<section>
 			<h2>.pp-hidden--mobile</h2>
-			<p style="background-color: #eee; padding: 1rem 0.5rem;">
-				<span class="pp-hidden--mobile">This message is hidden.</span>&nbsp;
-			</p>
+			<p class="vue-hidden-p"><span class="pp-hidden--mobile">This message is hidden.</span>&nbsp;</p>
 		</section>
 
 		<section>
 			<h2>.pp-hidden--tablet</h2>
-			<p style="background-color: #eee; padding: 1rem 0.5rem;">
-				<span class="pp-hidden--tablet">This message is hidden.</span>&nbsp;
-			</p>
+			<p class="vue-hidden-p"><span class="pp-hidden--tablet">This message is hidden.</span>&nbsp;</p>
 		</section>
 
 		<section>
 			<h2>.pp-hidden--desktop</h2>
-			<p style="background-color: #eee; padding: 1rem 0.5rem;">
-				<span class="pp-hidden--desktop">This message is hidden.</span>&nbsp;
-			</p>
+			<p class="vue-hidden-p"><span class="pp-hidden--desktop">This message is hidden.</span>&nbsp;</p>
 		</section>
 	</div>
 </template>
+
+<style>
+	.vue-hidden-p {
+		background-color: #eee;
+		padding: 1rem 0.5rem;
+	}
+
+	html.dark .vue-hidden-p {
+		background-color: #555;
+	}
+</style>
