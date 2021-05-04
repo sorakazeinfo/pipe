316192c16d6aafa278d4da0260a4c5515823cf73
Hyte Sorakaze <github@sorakaze.info>
2021-05-04 20:55:48 +0900

    Fix button style.

--- docs/src/components/agreement.vue
+++ docs/src/components/agreement.vue
@@ -8,7 +8,7 @@
 				<div><label><input type="checkbox"> Please agreement the privacy policy.</label></div>
 			</div>
 
-			<p><button type="button" id="submit" disabled="disabled">Submit</button></p>
+			<p><button type="button" id="submit" disabled="disabled">Submit(&lt;button&gt;)</button></p>
 		</div>
 
 		<div>
@@ -16,7 +16,15 @@
 				<div><label><input type="checkbox"> Please agreement the term of service.</label></div>
 			</div>
 
-			<p><input type="button" id="submit2" disabled="disabled" value="Submit"></p>
+			<p><input type="button" id="submit2" disabled="disabled" value="Submit([type=button])"></p>
+		</div>
+
+		<div>
+			<div data-pipejs="agreement" data-pipe-target="#submit3">
+				<div><label><input type="checkbox"> Please agreement the term of service.</label></div>
+			</div>
+
+			<p><input type="submit" id="submit3" disabled="disabled" value="Submit([type=submit])"></p>
 		</div>
 
 	</div>
